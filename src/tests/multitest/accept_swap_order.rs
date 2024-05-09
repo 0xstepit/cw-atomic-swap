d77cf91f4988c9ec67d111a868b478c5d2fee43b
stepit <stefanofrancesco.pitton@gmail.com>
2024-05-09 10:35:54 +0200

    fix cancelled tests

--- src/tests/multitest/accept_swap_order.rs
+++ src/tests/multitest/accept_swap_order.rs
@@ -1,8 +1,8 @@
 use cosmwasm_std::{Addr, Coin, Empty, Uint128};
 use cw_multi_test::{AppBuilder, BankSudo, Contract, ContractWrapper, Executor, SudoMsg};
 
-use crate::msg::ExecuteMsg;
-use crate::msg::{AllSwapOrdersResponse, InstantiateMsg, QueryMsg};
+use crate::msg::{ExecuteMsg, SwapOrdersByMakerResponse};
+use crate::msg::{InstantiateMsg, QueryMsg};
 use crate::state::{OrderStatus, SwapOrder};
 use crate::tests::multitest::stargate_handler::CustomStargate;
 
@@ -21,7 +21,7 @@ pub fn atomic_swap_market_contract() -> Box<dyn Contract<Empty>> {
 }
 
 #[test]
-fn test_confirm_order() {
+fn test_accept_swap_order_no_taker() {
     let mut app = AppBuilder::new()
         .with_stargate(CustomStargate::default())
         .build(|_, _, _| {});
@@ -86,45 +86,189 @@ fn test_confirm_order() {
         &[Coin::new(1_000, "usdc")],
     )
     .unwrap();
+}
 
-    let current_block_time = app.block_info().time.seconds();
-    let resp_all: AllSwapOrdersResponse = app
-        .wrap()
-        .query_wasm_smart(market_addr.clone(), &QueryMsg::AllSwapOrders {})
+#[test]
+fn test_accept_swap_order_with_taker() {
+    let mut app = AppBuilder::new()
+        .with_stargate(CustomStargate::default())
+        .build(|_, _, _| {});
+
+    let owner = Addr::unchecked(OWNER);
+    let stepit = Addr::unchecked("0xstepit".to_string());
+    let maker = Addr::unchecked("maker".to_string());
+
+    // Store and instantiate the market contract.
+    let market_id = app.store_code(atomic_swap_market_contract());
+    let init_market_msg = InstantiateMsg {
+        owner: Some(OWNER.to_string()),
+    };
+    let market_addr = app
+        .instantiate_contract(
+            market_id,
+            owner.clone(),
+            &init_market_msg,
+            &[],
+            "atomic-swap-market",
+            None,
+        )
         .unwrap();
-    assert_eq!(resp_all.orders.len(), 1, "expected one orders");
-    assert_eq!(
-        resp_all.orders[0].1,
-        SwapOrder {
-            coin_in: Coin::new(1_000, "uosmo"),
-            coin_out: Coin::new(1_000, "usdc"),
-            taker: Some(stepit.clone()),
-            status: OrderStatus::Accepted,
-            timeout: 10 + current_block_time,
-        },
-        "expected a different order status"
-    );
 
-    let resp = app.wrap().query_balance(maker.clone(), "usdc").unwrap();
-    assert_eq!(resp, Coin::new(0, "usdc"),);
-    let resp = app.wrap().query_balance(stepit.clone(), "uosmo").unwrap();
-    assert_eq!(resp, Coin::new(0, "uosmo"),);
+    // Mint tokens to maker and taker
+    let mut coin = Coin {
+        denom: "uosmo".to_string(),
+        amount: Uint128::new(1_000),
+    };
+    app.sudo(SudoMsg::Bank(BankSudo::Mint {
+        to_address: maker.to_string(),
+        amount: vec![coin.clone()],
+    }))
+    .unwrap();
 
-    // Confirm the order
-    let accept_order_msg = ExecuteMsg::ConfirmSwapOrder {
+    coin.denom = "usdc".to_string();
+    app.sudo(SudoMsg::Bank(BankSudo::Mint {
+        to_address: stepit.to_string(),
+        amount: vec![coin],
+    }))
+    .unwrap();
+
+    // Create first order.
+    let create_order_msg = ExecuteMsg::CreateSwapOrder {
+        coin_in: Coin::new(1_000, "uosmo"),
+        coin_out: Coin::new(1_000, "usdc"),
+        taker: Some(stepit.to_string()),
+        timeout: 10,
+    };
+    app.execute_contract(maker.clone(), market_addr.clone(), &create_order_msg, &[])
+        .unwrap();
+
+    // Accept the order
+    let accept_order_msg = ExecuteMsg::AcceptSwapOrder {
         order_id: 0,
         maker: maker.to_string(),
     };
     app.execute_contract(
-        maker.clone(),
+        stepit.clone(),
         market_addr.clone(),
         &accept_order_msg,
-        &[Coin::new(1_000, "uosmo")],
+        &[Coin::new(1_000, "usdc")],
     )
     .unwrap();
 
-    let resp = app.wrap().query_balance(maker.clone(), "usdc").unwrap();
-    assert_eq!(resp, Coin::new(1_000, "usdc"),);
-    let resp = app.wrap().query_balance(stepit.clone(), "uosmo").unwrap();
-    assert_eq!(resp, Coin::new(1_000, "uosmo"),);
+    let resp: SwapOrdersByMakerResponse = app
+        .wrap()
+        .query_wasm_smart(
+            market_addr.clone(),
+            &QueryMsg::SwapOrdersByMaker {
+                maker: maker.to_string(),
+            },
+        )
+        .unwrap();
+
+    let current_block_time = app.block_info().time.seconds();
+    assert_eq!(
+        resp.orders[0].1,
+        SwapOrder {
+            coin_in: Coin::new(1_000, "uosmo"),
+            coin_out: Coin::new(1_000, "usdc"),
+            taker: Some(stepit),
+            timeout: 10 + current_block_time,
+            status: OrderStatus::Accepted,
+        },
+        "expected the order to be accepted"
+    )
+}
+
+#[test]
+fn test_accept_swap_order_trigger_reply() {
+    let mut app = AppBuilder::new()
+        .with_stargate(CustomStargate::default())
+        .build(|_, _, _| {});
+
+    let owner = Addr::unchecked(OWNER);
+    let stepit = Addr::unchecked("0xstepit".to_string());
+    let maker = Addr::unchecked("maker".to_string());
+
+    // Store and instantiate the market contract.
+    let market_id = app.store_code(atomic_swap_market_contract());
+    let init_market_msg = InstantiateMsg {
+        owner: Some(OWNER.to_string()),
+    };
+    let market_addr = app
+        .instantiate_contract(
+            market_id,
+            owner.clone(),
+            &init_market_msg,
+            &[],
+            "atomic-swap-market",
+            None,
+        )
+        .unwrap();
+
+    // Mint tokens to maker and taker
+    let mut coin = Coin {
+        denom: "uosmo".to_string(),
+        amount: Uint128::new(1_000),
+    };
+    app.sudo(SudoMsg::Bank(BankSudo::Mint {
+        to_address: maker.to_string(),
+        amount: vec![coin.clone()],
+    }))
+    .unwrap();
+
+    coin.denom = "usdc".to_string();
+    app.sudo(SudoMsg::Bank(BankSudo::Mint {
+        to_address: stepit.to_string(),
+        amount: vec![coin],
+    }))
+    .unwrap();
+
+    // Create first order.
+    let create_order_msg = ExecuteMsg::CreateSwapOrder {
+        coin_in: Coin::new(1_000, "uosmo"),
+        coin_out: Coin::new(1_000, "usdc"),
+        taker: Some(stepit.to_string()),
+        timeout: 10,
+    };
+    app.execute_contract(maker.clone(), market_addr.clone(), &create_order_msg, &[])
+        .unwrap();
+
+    // Accept the order
+    let accept_order_msg = ExecuteMsg::AcceptSwapOrder {
+        order_id: 0,
+        maker: maker.to_string(),
+    };
+
+    let mut current_block = app.block_info();
+    let current_block_time = current_block.time.seconds();
+    current_block.height = 1;
+    app.set_block(current_block);
+    app.execute_contract(
+        stepit.clone(),
+        market_addr.clone(),
+        &accept_order_msg,
+        &[Coin::new(1_000, "usdc")],
+    )
+    .unwrap();
+
+    let resp: SwapOrdersByMakerResponse = app
+        .wrap()
+        .query_wasm_smart(
+            market_addr.clone(),
+            &QueryMsg::SwapOrdersByMaker {
+                maker: maker.to_string(),
+            },
+        )
+        .unwrap();
+    assert_eq!(
+        resp.orders[0].1,
+        SwapOrder {
+            coin_in: Coin::new(1_000, "uosmo"),
+            coin_out: Coin::new(1_000, "usdc"),
+            taker: Some(stepit),
+            timeout: 10 + current_block_time,
+            status: OrderStatus::Failed,
+        },
+        "expected the order to be failed because error in submessage"
+    );
 }
--- src/tests/multitest/confirm_swap_order.rs
+++ src/tests/multitest/confirm_swap_order.rs
@@ -1,31 +1,39 @@
 use cosmwasm_std::{Addr, Coin, Empty, Uint128};
-use cw_multi_test::{App, BankSudo, Contract, ContractWrapper, Executor, SudoMsg};
+use cw_multi_test::{AppBuilder, BankSudo, Contract, ContractWrapper, Executor, SudoMsg};
 
-use crate::msg::{
-    AllSwapOrdersResponse, ExecuteMsg, InstantiateMsg, QueryMsg, SwapOrdersByMakerResponse,
-};
+use crate::msg::ExecuteMsg;
+use crate::msg::{AllSwapOrdersResponse, InstantiateMsg, QueryMsg};
+use crate::state::{OrderStatus, SwapOrder};
+use crate::tests::multitest::stargate_handler::CustomStargate;
 
+const OWNER: &str = "0xstepit000";
+
+// Creates a market contract.
 pub fn atomic_swap_market_contract() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
         crate::contract::execute,
         crate::contract::instantiate,
         crate::contract::query,
-    );
+    )
+    .with_reply_empty(crate::contract::reply);
+
     Box::new(contract)
 }
 
 #[test]
-fn test_create_order() {
-    let mut app: App = App::default();
+fn test_confirm_swap_order() {
+    let mut app = AppBuilder::new()
+        .with_stargate(CustomStargate::default())
+        .build(|_, _, _| {});
 
-    let owner = Addr::unchecked("0xowner".to_string());
+    let owner = Addr::unchecked(OWNER);
     let stepit = Addr::unchecked("0xstepit".to_string());
     let maker = Addr::unchecked("maker".to_string());
 
     // Store and instantiate the market contract.
     let market_id = app.store_code(atomic_swap_market_contract());
     let init_market_msg = InstantiateMsg {
-        owner: Some("0xowner".to_string()),
+        owner: Some(OWNER.to_string()),
     };
     let market_addr = app
         .instantiate_contract(
@@ -38,8 +46,8 @@ fn test_create_order() {
         )
         .unwrap();
 
-    // Mint tokens to two accounts.
-    let coin = Coin {
+    // Mint tokens to maker and taker
+    let mut coin = Coin {
         denom: "uosmo".to_string(),
         amount: Uint128::new(1_000),
     };
@@ -48,13 +56,15 @@ fn test_create_order() {
         amount: vec![coin.clone()],
     }))
     .unwrap();
+
+    coin.denom = "usdc".to_string();
     app.sudo(SudoMsg::Bank(BankSudo::Mint {
         to_address: stepit.to_string(),
         amount: vec![coin],
     }))
     .unwrap();
 
-    // Create first order
+    // Create first order.
     let create_order_msg = ExecuteMsg::CreateSwapOrder {
         coin_in: Coin::new(1_000, "uosmo"),
         coin_out: Coin::new(1_000, "usdc"),
@@ -64,57 +74,57 @@ fn test_create_order() {
     app.execute_contract(maker.clone(), market_addr.clone(), &create_order_msg, &[])
         .unwrap();
 
-    // Create second order with another account
-    let create_order_msg = ExecuteMsg::CreateSwapOrder {
-        coin_in: Coin::new(1_000, "uosmo"),
-        coin_out: Coin::new(1_000, "usdc"),
-        taker: None,
-        timeout: 100,
+    // Accept the order
+    let accept_order_msg = ExecuteMsg::AcceptSwapOrder {
+        order_id: 0,
+        maker: maker.to_string(),
     };
-    app.execute_contract(stepit.clone(), market_addr.clone(), &create_order_msg, &[])
-        .unwrap();
+    app.execute_contract(
+        stepit.clone(),
+        market_addr.clone(),
+        &accept_order_msg,
+        &[Coin::new(1_000, "usdc")],
+    )
+    .unwrap();
 
-    let resp: SwapOrdersByMakerResponse = app
-        .wrap()
-        .query_wasm_smart(
-            market_addr.clone(),
-            &QueryMsg::SwapOrdersByMaker {
-                maker: maker.to_string(),
-            },
-        )
-        .unwrap();
+    let current_block_time = app.block_info().time.seconds();
     let resp_all: AllSwapOrdersResponse = app
         .wrap()
         .query_wasm_smart(market_addr.clone(), &QueryMsg::AllSwapOrders {})
         .unwrap();
-
-    assert_eq!(resp.orders.len(), 1, "expected one order from the maker");
-    assert_eq!(resp_all.orders.len(), 2, "expected two orders");
-
-    // Let the order expire
-    app.update_block(|block| {
-        block.height += 1;
-        block.time = block.time.plus_seconds(11);
-    });
-
-    let resp: SwapOrdersByMakerResponse = app
-        .wrap()
-        .query_wasm_smart(
-            market_addr.clone(),
-            &QueryMsg::SwapOrdersByMaker {
-                maker: maker.to_string(),
-            },
-        )
-        .unwrap();
-    let resp_all: AllSwapOrdersResponse = app
-        .wrap()
-        .query_wasm_smart(market_addr.clone(), &QueryMsg::AllSwapOrders {})
-        .unwrap();
-
+    assert_eq!(resp_all.orders.len(), 1, "expected one orders");
     assert_eq!(
-        resp.orders.len(),
-        0,
-        "expected zero order from first maker because expired"
+        resp_all.orders[0].1,
+        SwapOrder {
+            coin_in: Coin::new(1_000, "uosmo"),
+            coin_out: Coin::new(1_000, "usdc"),
+            taker: Some(stepit.clone()),
+            status: OrderStatus::Accepted,
+            timeout: 10 + current_block_time,
+        },
+        "expected a different order status"
     );
-    assert_eq!(resp_all.orders.len(), 1, "expected one order still active");
+
+    let resp = app.wrap().query_balance(maker.clone(), "usdc").unwrap();
+    assert_eq!(resp, Coin::new(0, "usdc"),);
+    let resp = app.wrap().query_balance(stepit.clone(), "uosmo").unwrap();
+    assert_eq!(resp, Coin::new(0, "uosmo"),);
+
+    // Confirm the order
+    let accept_order_msg = ExecuteMsg::ConfirmSwapOrder {
+        order_id: 0,
+        maker: maker.to_string(),
+    };
+    app.execute_contract(
+        maker.clone(),
+        market_addr.clone(),
+        &accept_order_msg,
+        &[Coin::new(1_000, "uosmo")],
+    )
+    .unwrap();
+
+    let resp = app.wrap().query_balance(maker.clone(), "usdc").unwrap();
+    assert_eq!(resp, Coin::new(1_000, "usdc"),);
+    let resp = app.wrap().query_balance(stepit.clone(), "uosmo").unwrap();
+    assert_eq!(resp, Coin::new(1_000, "uosmo"),);
 }
